88ca645930a7ef6944b71bde067ab4d98b06583a
Joel <joconno07@gmail.com>
2020-03-19 15:20:45 -0600

    Basics of typescript

---
+++ typescript-practice.ts
@@ -0,0 +1,15 @@
+let myString: string;
+// myString = 4;
+myString = 'hello';
+
+
+let anotherString = 'another string';
+// anotherString = 4;
+
+
+let yetAnotherString;
+yetAnotherString = 'strings';
+yetAnotherString = 5;
+
+
+
